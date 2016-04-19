ef0980f6d787c9f1e6b41cf237895821a37d5ee5
ferrantejake <jake.ferrante@hotmail.com>
2016-04-19 15:36:46 -0400

    in progress kinda messy just checking in

--- project.c
+++ project.c
@@ -1,13 +1,12 @@
-
 #include "spimcore.h"
-/* ALU 
-1.	Implement the operations on input parameters A and B according to ALUControl.
-2.	Output the result (Z) to ALUresult.
-3.	Assign Zero to 1 if the result is zero; otherwise, assign 0.
-4.	The following table shows the operations of the ALU.
-*/
+
 void ALU(unsigned A, unsigned B, char ALUControl, unsigned *ALUresult, char *Zero) 
 {
+	/* ALU 
+		1.	Implement the operations on input parameters A and B according to ALUControl.
+		2.	Output the result (Z) to ALUresult.
+		3.	Assign Zero to 1 if the result is zero; otherwise, assign 0.
+	*/
 	switch (ALUControl) {
 	case 0x0:		// Add A + B  || don't care??
 		*ALUresult = A + B;
@@ -36,54 +35,90 @@ void ALU(unsigned A, unsigned B, char ALUControl, unsigned *ALUresult, char *Zer
 		break;
 	case 0x7:		// Z = NOT A
 		*ALUresult = ~A;
-
 		break;
 	}
-	if(ALUresult * 0x0)
+	if(ALUresult = 0)
 		*Zero = 1;
 	*Zero = 0;
 }
 
-/*
-If one of the following situations is encountered, the global flag Halt is set to 1, and hence the simulation halts.
- 	An illegal instruction is encountered.
- 	Jumping to an address that is not word-aligned (being multiple of 4).
- 	The address of lw or sw is not word-aligned.
-  	Accessing data or jump to address that is beyond the memory.
-Note: The instructions beyond the list of instructions in Figure 1 are illegal.
-*/
 int instruction_fetch(unsigned PC, unsigned *Mem, unsigned *instruction)
 {
-	// No halt condition has been met here. This needs to be addressed
-	*instruction = *Mem + 4 * PC;
-	return 0;
+	/* 
+		If one of the following situations is encountered, the global flag Halt is set to 1, and hence the simulation halts.
+		An illegal instruction is encountered.
+		Jumping to an address that is not word-aligned (being multiple of 4).
+		The address of lw or sw is not word-aligned.
+		Accessing data or jump to address that is beyond the memory.
+		Note: The instructions beyond the list of instructions in Figure 1 are illegal. 
+		greater than or equal than 0, within range, and PC mod 4  = 0;
+		PC needs to be cast to signed to compare to the decimal values
+	*/
+	if((signed) PC >= 0 && PC % 4 == 0 && (signed) PC <= 65535) {
+		*instruction = Mem[4 * PC];
+		return 0;
+	}
+	return 1;
 }
 
-/*
-1.	Fetch the instruction addressed by PC from Mem and write it to instruction.
-2.	Return 1 if a halt condition occurs; otherwise, return 0.
-*/
 void instruction_partition(unsigned instruction, unsigned *op, unsigned *r1, unsigned *r2, unsigned *r3, unsigned *funct, unsigned *offset, unsigned *jsec)
 {
-	/* Required Instructions
-	Hex			Command				Type
-	0x000000 // add					R
-	0x000000 // sub					R
-	0x001000 // addi				I
-	0x000000 // and					R
-	0x000000 // or					R
-	0x100011 // lw load word		I
-	0x101011 // sw store word		I
-	0x001111 // lui					I
-	0x000100 // branch on eq		I
-	0x000000 // slt					R
-	0x001010 // slti				I
-	0x000000 // sltu				R
-	0x001011 // sltiu				I
-	0x000010 // jump				J
+	/*
+		1.	Fetch the instruction addressed by PC from Mem and write it to instruction.
+		2.	Return 1 if a halt condition occurs; otherwise, return 0.
 	*/
+	*op = ((instruction >> 26) & 0x3F); // mask 6 bits
+	*r1 = ((instruction >> 21) & 0x1F); // mask 5 bits
+	*r2 = ((instruction >> 16) & 0x1F);
+	*r3 = ((instruction >> 11) & 0x1F);
+	*funct = instruction & 26;
+	*offset = instruction & 26;
+	*jsec = instruction & 2
+}
 
-	*op = instruction >> 26;
+
+
+
+
+int instruction_decode(unsigned op, struct_controls *controls) 
+{
+	/*
+		1.	Decode the instruction using the opcode (op).
+		2.	Assign the values of the control signals to the variables in the structure controls (See spimcore.h file).
+		The meanings of the values of the control signals:
+		For MemRead, MemWrite or RegWrite, the value 1 means that enabled, 0 means that disabled, 2 means �don�t care�.
+		For RegDst, Jump, Branch, MemtoReg or ALUSrc, the value 0 or 1 indicates the selected path of the multiplexer; 2 means �don�t care�.
+		The following table shows the meaning of the values of ALUOp.
+		3.	Return 1 if a halt condition occurs; otherwise, return 0.
+		
+		// initially set all of the controls to off
+		*controls->RegDst = 0;
+		*controls->Jump = 0;
+		*controls->Branch = 0;
+		*controls->MemRead = 0;
+		*controls->MemtoReg = 0;
+		*controls->ALUOp = 1;
+		*controls->MemWrite = 0; 
+		*controls->ALUSrc = 0;
+		*controls->RegWrite = 0;
+	*/
+	
+	/* Required Instructions
+		Hex			Command				Type
+		0x000000 // add					R
+		0x000000 // sub					R
+		0x000000 // and					R
+		0x000000 // or					R
+		0x000000 // slt					R
+		0x000000 // sltu				R
+		0x001000 // addi				I
+		0x100011 // lw load word		I
+		0x101011 // sw store word		I
+		0x001111 // lui					I
+		0x000100 // branch on eq		I
+		0x001010 // slti				I
+		0x001011 // sltiu				I
+		0x000010 // jump				J
 
 	/* R-Type */
 	if (*op == 0x0) {
@@ -116,44 +151,20 @@ void instruction_partition(unsigned instruction, unsigned *op, unsigned *r1, uns
 	else
 		return 1; // hey that's bad HALT!
 	return 0; // everything is okay
-}
-
-
-/*
-1.	Decode the instruction using the opcode (op).
-2.	Assign the values of the control signals to the variables in the structure controls (See spimcore.h file).
-The meanings of the values of the control signals:
-For MemRead, MemWrite or RegWrite, the value 1 means that enabled, 0 means that disabled, 2 means �don�t care�.
-For RegDst, Jump, Branch, MemtoReg or ALUSrc, the value 0 or 1 indicates the selected path of the multiplexer; 2 means �don�t care�.
-The following table shows the meaning of the values of ALUOp.
-3.	Return 1 if a halt condition occurs; otherwise, return 0.
-*/
-int instruction_decode(unsigned op, struct_controls *controls) 
-{
-
-	/*
-	{	
-		Below these controls need to be set to 1 or 0 according to the instruction
-		that is being read in from the op code except for RegDest which is an address:
+	*/	
 		
-		char RegDst; 
-		char Jump; 
-		char Branch; 
-		char MemRead; 
-		char MemtoReg; 
-		char ALUOp; 
-		char MemWrite; 
-		char ALUSrc; 
-		char RegWrite; 
-	}struct_controls;
-	*/
+
+		
+		/*take in the op code and see if the instruction is an 
+	R , I, or J type and set op codes 
+	0 : Disabled 
+	1 : enabled
+	2 : Don't Care */
 	
 	switch( op )
-	{
-		
-		
-		case 0x0:
-		{// addition or don't care
+	{	
+		case 0x0: // R-Type
+		{
 			
 		}
 		case 0x1:
@@ -168,7 +179,7 @@ int instruction_decode(unsigned op, struct_controls *controls)
 		{//set less than unsigned
 			
 		}
-		case 0x4:
+		case 0x4: // branch if equal
 		{//and operator 
  			
 		}
@@ -184,34 +195,32 @@ int instruction_decode(unsigned op, struct_controls *controls)
 		{ // rtypes 
 			
 		}	
+				
+		0x001000 // addi		
+		0x100011 // lw load word
+		0x101011 // sw store word
+		0x001111 // lui			
+		0x000100 // branch on eq
+		0x001010 // slti		
+		0x001011 // sltiu		
+		0x000010 // jump		
+	
 	}// end of switch 
 	
 	
 	
 		
-	/*take in the op code and see if the instruction is an 
-	R , I, or J type and set op codes 
-	1 : enabled
-	0 : Disabled 
-	2 : Don't Care */
+	
 	
 	return 0;
 }
 
-
-/*
-1.	Read the registers addressed by r1 and r2 from Reg, and write the read values to data1 and data2 respectively.
-*/
 void read_register(unsigned r1, unsigned r2, unsigned *Reg, unsigned *data1, unsigned *data2) 
 {
 	*data1 = r1; // assign r1 to data 1 
 	*data2 = r2; // assign r2 to data 2
 }
 
-
-/*
-1.	Assign the sign-extended value of offset to extended_value.
-*/
 void sign_extend(unsigned offset, unsigned *extended_value) 
 {
 	int first_digit = offset >> 15;
@@ -221,16 +230,16 @@ void sign_extend(unsigned offset, unsigned *extended_value)
 		*extended_value = offset | 0x00000000;	// OR with 0 which will return with leading 0s        
 }
 
-
 /* ALU operations */ /* 10 Points */
 int ALU_operations(unsigned data1, unsigned data2, unsigned extended_value, unsigned funct, char ALUOp, char ALUSrc, unsigned *ALUresult, char *Zero) 
 {
+	if(ALUOp == 1)
+		data2 = extended_value;
+	
+	ALU(data1, data2, char funct, unsigned *ALUresult, char *Zero) 
 	return 0;
 }
 
-
-/* Read / Write Memory */
-/* 10 Points */
 int rw_memory(unsigned ALUresult, unsigned data2, char MemWrite, char MemRead, unsigned *memdata, unsigned *Mem) 
 {
 	if( MemRead && MemWrite)
@@ -250,16 +259,16 @@ int rw_memory(unsigned ALUresult, unsigned data2, char MemWrite, char MemRead, u
 	return 0;
 }
 
-
-/* Write Register */ /* 10 Points */
 void write_register(unsigned r2, unsigned r3, unsigned memdata, unsigned ALUresult, char RegWrite, char RegDst, char MemtoReg, unsigned *Reg) 
 {
+	// write all data to ALUresult or memdata to a register addressed by r2 or r3
 }
 
-/* PC update */ /* 10 Points */
 void PC_update(unsigned jsec, unsigned extended_value, char Branch, char Jump, char Zero, unsigned *PC) 
 {
 	if(Jump)
 		*PC = jsec*4;
-	if(Branch )
+	else if(Branch)
+		*PC + jsec * 4;
+	else *PC+=4;
 }
