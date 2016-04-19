7f72adcb44e971c0ce1b1f3eed55ccf5e35a42a9
ferrantejake <jake.ferrante@hotmail.com>
2016-04-19 18:02:00 -0400

    updates to ALU controls and more

--- project.c
+++ project.c
@@ -63,17 +63,17 @@ int instruction_fetch(unsigned PC, unsigned *Mem, unsigned *instruction)
 
 void instruction_partition(unsigned instruction, unsigned *op, unsigned *r1, unsigned *r2, unsigned *r3, unsigned *funct, unsigned *offset, unsigned *jsec)
 {
-	/*
-		1.	Fetch the instruction addressed by PC from Mem and write it to instruction.
-		2.	Return 1 if a halt condition occurs; otherwise, return 0.
-	*/
-	*op = ((instruction >> 26) & 0x3F); // mask 6 bits
-	*r1 = ((instruction >> 21) & 0x1F); // mask 5 bits
-	*r2 = ((instruction >> 16) & 0x1F);
-	*r3 = ((instruction >> 11) & 0x1F);
-	*funct = instruction & 26;
-	*offset = instruction & 26;
-	*jsec = instruction & 2
+	const 5_BITS = 0x1F;
+	const 6_BITS = 0x3F;
+	const 16_BITS = 0xFFFF;
+	const 26_BITS = 0x3FFFFFF;	
+	*op = ((instruction >> 26) & 6_BITS);
+	*r1 = ((instruction >> 21) & 5_BITS);
+	*r2 = ((instruction >> 16) & 5_BITS);
+	*r3 = ((instruction >> 11) & 5_BITS);
+	*funct = instruction & 6_BITS);
+	*offset = instruction & 16_BITS;
+	*jsec = instruction & 26_BITS);
 }
 
 
@@ -90,17 +90,6 @@ int instruction_decode(unsigned op, struct_controls *controls)
 		For RegDst, Jump, Branch, MemtoReg or ALUSrc, the value 0 or 1 indicates the selected path of the multiplexer; 2 means �don�t care�.
 		The following table shows the meaning of the values of ALUOp.
 		3.	Return 1 if a halt condition occurs; otherwise, return 0.
-		
-		// initially set all of the controls to off
-		*controls->RegDst = 0;
-		*controls->Jump = 0;
-		*controls->Branch = 0;
-		*controls->MemRead = 0;
-		*controls->MemtoReg = 0;
-		*controls->ALUOp = 1;
-		*controls->MemWrite = 0; 
-		*controls->ALUSrc = 0;
-		*controls->RegWrite = 0;
 	*/
 	
 	/* Required Instructions
@@ -119,17 +108,62 @@ int instruction_decode(unsigned op, struct_controls *controls)
 		0x001010 // slti				I
 		0x001011 // sltiu				I
 		0x000010 // jump				J
+	
+	
+	Notes
+	i-type will always require ALUsrc which is enabled on a sign extended.
+	j-type will not be sign extended
+*/
+	switch(op) {
+		case 0x000000: // r-type
+			break;
+		case 0x001000: // addi
+			*controls->RegDst = 0;
+			*controls->Jump = 0;
+			*controls->Branch = 0;
+			*controls->MemRead = 0;
+			*controls->MemtoReg = 0;
+			*controls->ALUOp = 0;
+			*controls->MemWrite = 0; 
+			*controls->ALUSrc = 1; // because it's an i typ[e]
+			*controls->RegWrite = 1;
+			break;
+		case 0x100011:
+			break;
+		case 0x101011:
+			break;
+		case 0x001111:
+			break;
+		case 0x000100:
+			break;
+		case 0x001010:
+			break;
+		case 0x001011:
+			break;
+		case 0x000010:
+			break;
+		default:
+			return 1;
+			break;
+	}
+		
 
 	/* R-Type */
 	if (*op == 0x0) {
-		*r1 = ((instruction >> 21) & 0x1F);
-		*r2 = ((instruction >> 16) & 0x1F);
-		*r3 = ((instruction >> 11) & 0x1F);
-		*offset = ((instruction >> 6) & 0x1F);
-		*funct = instruction & 0x3F;
+		*controls->RegDst = 1;
+		*controls->ALUSrc = 0;
+		*controls->MemtoReg = 0;
+		*controls->RegWrite = 1;
+		*controls->MemRead = 0;
+		*controls->MemWrite = 0; 
+		*controls->Branch = 0;
+		*controls->Jump = 0;
+		*controls->ALUOp = 1;
 	}
 
-	/* J-Type */
+	/* LW */
+	/* LW */
+	
 	else if (*op == 0x10) { // if jump 
 		*jsec = (instruction & 0x3FFFFFF);
 	}
@@ -151,62 +185,7 @@ int instruction_decode(unsigned op, struct_controls *controls)
 	else
 		return 1; // hey that's bad HALT!
 	return 0; // everything is okay
-	*/	
-		
 
-		
-		/*take in the op code and see if the instruction is an 
-	R , I, or J type and set op codes 
-	0 : Disabled 
-	1 : enabled
-	2 : Don't Care */
-	
-	switch( op )
-	{	
-		case 0x0: // R-Type
-		{
-			
-		}
-		case 0x1:
-		{// subtraction
-			
-		}
-		case 0x2:
-		{// set on less than
-			
-		}
-		case 0x3:
-		{//set less than unsigned
-			
-		}
-		case 0x4: // branch if equal
-		{//and operator 
- 			
-		}
-		case 0x5:
-		{// or operator		
-			
-		}
-		case 0x6:
-		{ // shift left extended by 16 bits
-			
-		}
-		case 0x7:
-		{ // rtypes 
-			
-		}	
-				
-		0x001000 // addi		
-		0x100011 // lw load word
-		0x101011 // sw store word
-		0x001111 // lui			
-		0x000100 // branch on eq
-		0x001010 // slti		
-		0x001011 // sltiu		
-		0x000010 // jump		
-	
-	}// end of switch 
-	
 	
 	
 		
@@ -224,7 +203,7 @@ void read_register(unsigned r1, unsigned r2, unsigned *Reg, unsigned *data1, uns
 void sign_extend(unsigned offset, unsigned *extended_value) 
 {
 	int first_digit = offset >> 15;
-	if (first_digit)							// if negative then
+	if ((signed) first_digit) // if negative then
 		*extended_value = offset | 0xFFFF0000;	// OR with 32 bit with leading 16 1s 
 	else										// otherwise positive
 		*extended_value = offset | 0x00000000;	// OR with 0 which will return with leading 0s        
@@ -234,7 +213,7 @@ void sign_extend(unsigned offset, unsigned *extended_value)
 int ALU_operations(unsigned data1, unsigned data2, unsigned extended_value, unsigned funct, char ALUOp, char ALUSrc, unsigned *ALUresult, char *Zero) 
 {
 	if(ALUOp == 1)
-		data2 = extended_value;
+		data2 = extended_value; // because its an i type
 	
 	ALU(data1, data2, char funct, unsigned *ALUresult, char *Zero) 
 	return 0;
